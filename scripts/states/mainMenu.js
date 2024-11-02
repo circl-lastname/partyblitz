d440d6ce0c6297a199b75c3c4b49d249d203148d
circl <circl.lastname@gmail.com>
2024-11-02 13:07:49 +0100

    Draw username on mainMenu

--- scripts/states/mainMenu.js
+++ scripts/states/mainMenu.js
@@ -1,10 +1,26 @@
 states.mainMenu = {};
+states.mainMenu.playerDataHooks = {};
 
 states.mainMenu.enter = function () {
+  if (playerData.username) {
+    let metrics = rendering.ctx.measureText(playerData.username);
+    localStateData.usernameX = 1280/2 - metrics.width/2;
+    localStateData.usernameY = 720/2 - metrics.actualBoundingBoxDescent/2;
+  }
+};
+
+states.mainMenu.playerDataHooks.username = function () {
+  let metrics = rendering.ctx.measureText(playerData.username);
+  localStateData.usernameX = 1280/2 - metrics.width/2;
+  localStateData.usernameY = 720/2 - metrics.actualBoundingBoxDescent/2;
   
+  rendering.scheduleRender();
 };
 
 states.mainMenu.render = function () {
   rendering.ctx.fillStyle = "#ffffff";
   rendering.fillBackground();
+  
+  rendering.ctx.fillStyle = "#000000";
+  rendering.ctx.fillText(playerData.username, localStateData.usernameX, localStateData.usernameY);
 };
