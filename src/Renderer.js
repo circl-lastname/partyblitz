f6633f3d38e3543d53699e96fe39dd616948df48
circl <circl.lastname@gmail.com>
2025-08-11 18:44:34 +0200

    Import renderer from Kethrun

---
+++ src/Renderer.js
@@ -0,0 +1,52 @@
+// Do not destroy this class
+export class Renderer {
+  element;
+  ctx;
+  width;
+  height;
+  
+  fps = 0;
+  #fpsCounter = 0;
+  
+  frameCallback;
+  
+  constructor() {
+    this.element = document.querySelector("canvas");
+    this.ctx = this.element.getContext("2d");
+    this.#handleResize();
+    
+    window.addEventListener("resize", () => this.#handleResize());
+    requestAnimationFrame((t) => this.#handleFrame(t));
+    setInterval(() => {
+      this.fps = this.#fpsCounter;
+      this.#fpsCounter = 0;
+    }, 1000);
+  }
+  
+  #handleResize() {
+    this.element.style.width = `${window.innerWidth}px`;
+    this.element.style.height = `${window.innerHeight}px`;
+    
+    this.element.width = Math.round(window.innerWidth * window.devicePixelRatio);
+    this.element.height = Math.round(window.innerHeight * window.devicePixelRatio);
+    
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    
+    this.ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+    
+    this.ctx.font = "16px sans-serif";
+  }
+  
+  #handleFrame(t) {
+    if (!this.frameCallback) {
+      this.ctx.fillStyle = "#ff8000";
+      this.ctx.fillRect(0, 0, this.width, this.height);
+    } else {
+      this.frameCallback(t);
+    }
+    
+    this.#fpsCounter++;
+    requestAnimationFrame((t) => this.#handleFrame(t));
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { Renderer } from "/Renderer.js";
+
+console.log("Partyblitz");
+
+export const rend = new Renderer();
